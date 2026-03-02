defc75bc6ff43a3ea0549f7e11a9b9a9c4b7dc04
Sebbbenator <eaa25ssg@students.eaaa.dk>
2026-03-02 13:41:53 +0100

    Add 404 Not Found page styles
    - Created a new CSS file for the Not Found page with styles for layout, typography, and animations.
    - Included a background image for the link and hover effects.
    - Added keyframe animation for a spinning and bouncing effect on the image.

--- src/pages/NotFoundPage.jsx
+++ src/pages/NotFoundPage.jsx
@@ -1,17 +1,23 @@
 import { Link } from "react-router";
+import "../pages/not-found.css";
 
 export default function NotFoundPage() {
   return (
-    <>
-      <header>
-        <h1 className="not-found-title">404</h1>
+    <div className="not-found-page">
+      <header className="not-found-header">
+        <img
+          className="not-found-image"
+          src="../public/img/vindheks.svg"
+          alt="Not Found"
+        />
+        <h1 className="not-found-title">404 - Page Not Found</h1>
       </header>
       <main className="not-found">
-        <p>Sorry, the page you're looking for doesn't exist.</p>
+        {/* <p>Sorry, the page you're looking for doesn't exist.</p> */}
         <Link to="/" className="not-found-link">
-          Go back home
+          Go back
         </Link>
       </main>
-    </>
+    </div>
   );
 }
