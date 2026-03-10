bc7ebe6709eee4e1eb0a95a15bc799b3bae1a006
Magecties <magnusbbb@hotmail.com>
2026-03-10 12:23:52 +0100

    lightmode for pc card på homepage

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -2,8 +2,6 @@ import "../css/HomePage.css";
 import TheMostWantedText from "../components/homepage/themostwantedtext";
 import CardSlider from "../components/homepage/CardSlider";
 import ProductCarousel from "../components/homepage/ProductCarousel";
-import CategoryCardTop from "../components/homepage/CategoryCardTop";
-import CategoryCardBot from "../components/homepage/CategoryCardBot";
 import HeroImage from "../components/homepage/HeroImage";
 import Ticker from "../components/Ticker";
 import CategoryCard from "../components/homepage/CategoryCard";
