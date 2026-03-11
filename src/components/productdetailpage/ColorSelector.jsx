aa4a869520ab07e970d05ba9c7602ac6e2f07f33
Sebbbenator <eaa25ssg@students.eaaa.dk>
2026-03-11 13:21:52 +0100

    Styling af product detail, virker nu med light mode

--- src/components/productdetailpage/ColorSelector.jsx
+++ src/components/productdetailpage/ColorSelector.jsx
@@ -5,7 +5,7 @@ export default function ColorSelector({ colors, selected, onSelect }) {
 
   return (
     <div className="color-selector">
-      <p>
+      <p className="color-text">
         COLOR: <span>{selected}</span>
       </p>
       <div className="color-circles">
